7bb97ba89bf87f5b26bf29a662d6ff54afd5eacd
rayal1017 <sritharayal2004@gmail.com>
2025-05-06 17:04:45 +0530

    first commit

---
+++ src/test/java/com/ap/bbpms/baseball_management/BaseballManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.bbpms.baseball_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
